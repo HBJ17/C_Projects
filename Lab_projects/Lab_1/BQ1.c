7b175b86b0cdfe37b539a7d8c185f491cebc84e5
HBJ17 <hbj17672007@gmail.com>
2026-01-31 09:07:10 +0530

    completed BQ1

---
+++ Lab_projects/Lab_1/BQ1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char x;
+    
+    printf("Enter a character = ");
+    scanf("%c",&x);
+
+    if(x>=97 && x<=122) {
+        printf("Lowercase");
+    }
+    else if(x>=65 && x<=90) {
+        printf("Uppercase");
+    }
+    else if(x>=48 && x<=57) {
+        printf("Digit");
+    }
+    else {
+        printf("Special character");
+    }
+    return 0;
+}
+
