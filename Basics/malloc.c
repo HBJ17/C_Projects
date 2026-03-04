418d6df686317d41eb6d2ec142bb0e1c588657a1
HBJ17 <hbj17672007@gmail.com>
2026-03-04 21:50:55 +0530

    malloc

---
+++ Basics/malloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number;
+    int i;
+    printf("Enter the number of grades = ");
+    scanf("%d",&number);
+
+
+    char *grades = malloc(number * sizeof(char));
+
+    if (grades == NULL) {
+        printf("Memory Allocation failed!\n");
+        return 1;
+    }
+
+    for(i=0;i<number;i++) {
+        printf("Enter grade %d = ",i+1);
+        scanf(" %c",&grades[i]);
+    }
+
+    for(i=0;i<number;i++) {
+        printf("%c",grades[i]);
+    }
+
+    free(grades);
+    grades=NULL;
+
+    return 0;
+
+}
