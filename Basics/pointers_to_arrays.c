63f348f82110660ef13fa2e138129036821a17d3
HBJ17 <hbj17672007@gmail.com>
2026-03-25 05:00:42 +0530

    learnt pointers to arrays

---
+++ Basics/pointers_to_arrays.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3] = {10, 20, 30};
+    int (*p)[3] = &arr;
+
+    printf("Value stored in pointer %d \n",(p)[1]);
+    printf("Value stored in the address %d",(*p)[1]);
+
+    return 0;
+}
