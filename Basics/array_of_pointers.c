83cac255fdfae9d15264334e894b37452ae5dd6b
HBJ17 <hbj17672007@gmail.com>
2026-03-25 05:06:01 +0530

    learnt array of pointers and its working

---
+++ Basics/array_of_pointers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30;
+    int *arr[3] = {&a,&b,&c};
+
+    printf("The value stored in pointer = %d \n",arr[1]);
+    printf("The value stores in the address = %d",*arr[1]);
+
+    return 0;
+}
