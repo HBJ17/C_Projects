765ef955e56d23711d174b3b4b302d77024eac0a
HBJ17 <hbj17672007@gmail.com>
2026-03-30 17:45:41 +0530

    did sum of an array

---
+++ Basics/Arrays/array_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    int arr[5] = {1,2,3,4,5};
+    int i;
+
+    for(i=0;i<5;i++) {
+        sum = sum + arr[i];
+    }
+
+    printf("Sum is = %d",sum);
+
+    return 0;
+}
