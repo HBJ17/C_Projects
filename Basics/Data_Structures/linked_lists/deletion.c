36151445f991a7b84b7fedb2248ff2ddc50c2b94
HBJ17 <hbj17672007@gmail.com>
2026-04-23 18:14:12 +0530

    deletion in linkedlist

--- Basics/Data_Structures/linked_lists/deletion.c
+++ Basics/Data_Structures/linked_lists/deletion.c
@@ -19,6 +19,59 @@ struct Node* deleteBeginning(struct Node* head) {
     return head;
 }
 
+struct Node* deleteend(struct Node* head) {
+    if(head == NULL) {
+        printf("List is Empty");
+        return NULL;
+    }
+
+    if (head->next == NULL) {
+        free(head);
+        return NULL;
+    }
+
+    struct Node* temp = head;
+    while(temp->next->next != NULL) {
+        temp = temp->next;
+    }
+
+    free(temp->next);
+    temp->next = NULL;
+
+    return head;
+}
+
+struct Node* deleteatposition(struct Node* head,int pos) {
+
+    if(head == NULL) {
+        printf("The List is empty");
+        return NULL;
+    }
+
+    if(pos == 1) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    struct Node* temp = head;
+    for(int p = 1; p < pos-1 && temp->next != NULL; p++) {
+        temp = temp->next;
+    }
+
+    if(temp->next == NULL) {
+        printf("Position out of bounds");
+        return head;
+    }
+
+    struct Node* nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    free(nodeToDelete);
+
+    return head;
+}
+
 int main() {
     int n,i;
 
@@ -75,6 +128,36 @@ int main() {
     }
     printf(" -> NULL");
 
+    printf("\nDeleting at end\n");
+    head = deleteend(head);
+
+    printf("Linked LIsts\n");
+    temp = head;
+    while(temp!=NULL) {
+        printf("%d",temp->data);
+        if(temp->next != NULL) {
+            printf(" -> ");
+        }
+        temp = temp->next;
+    }
+    printf(" -> NULL");
+
+    printf("\nDeleting at a position\n");
+    printf("Enter the position to delete = ");
+    int k;
+    scanf("%d",&k);
+    head = deleteatposition(head, k);
+
+    printf("Linked LIsts\n");
+    temp = head;
+    while(temp!=NULL) {
+        printf("%d",temp->data);
+        if(temp->next != NULL) {
+            printf(" -> ");
+        }
+        temp = temp->next;
+    }
+    printf(" -> NULL");
 
     free(temp);
     free(head);
