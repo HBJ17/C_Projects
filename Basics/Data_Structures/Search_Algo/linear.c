5d748d93c1aeb16401175c25bff2bee868fc5949
HBJ17 <hbj17672007@gmail.com>
2026-04-28 19:55:22 +0530

    Searching Algorithms in C

--- Basics/Data_Structures/Search_Algo/linear.c
+++ Basics/Data_Structures/Search_Algo/linear.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int arr[] = {1,2,3,4,5,6};
-
-    int value,i;
-
-    printf("The array = ");
-    for(i = 0;i<(sizeof(arr)/sizeof(arr[0]));i++) {
-        printf(" %d",arr[i]);
-    }
-
-    printf("\nEnter the number you want to search = \n");
-    scanf("%d",&value);
-
-    printf("\nSearching\n");
-
-    for(i = 0;i<(sizeof(arr)/sizeof(arr[0]));i++) {
-        if(arr[i] == value) {
-            printf("\nValue is found at position %d\n",i+1);
-            return 0;
+int linearSearch(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            return i;  // element found
         }
     }
+    return -1;  // not found
+}
+
+int main() {
+    int arr[] = {10, 25, 30, 45, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+
+    int result = linearSearch(arr, n, key);
+
+    if(result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
 
     return 0;
-
 }
